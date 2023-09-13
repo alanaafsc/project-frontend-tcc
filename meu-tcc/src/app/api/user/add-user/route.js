61ed95cb35ebb0988164bf2de8f3a01ab094d87d
Alana Fernandes <48449273+alanaafsc@users.noreply.github.com>
2023-09-12 23:00:02 -0300

    att

--- meu-tcc/src/app/api/user/add-user/route.js
+++ meu-tcc/src/app/api/user/add-user/route.js
@@ -1,24 +1,22 @@
 import bcrypt from 'bcrypt';
-import { sql, transaction } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
-
+import { sql } from '@vercel/postgres';
+import { NextResponse, revalidatePath } from 'next/server';
+ 
 export async function POST(request) {
   const { name, email, password } = await request.json();
-
+ 
   try {
     if (!name || !email || !password) throw new Error('Name, email, and password are required');
-
-    // Iniciar uma transação
-    await transaction(async (db) => {
-      // Encriptar a senha antes de armazenar no banco de dados
-      const passwordHash = await bcrypt.hash(password, 10);
-
-      // Inserir o usuário na tabela dentro da transação
-      await db.query`INSERT INTO Users (name, email, password_hash) VALUES (${name}, ${email}, ${passwordHash});`;
-    });
-
-    return NextResponse.json({ message: 'User created successfully' }, { status: 201 });
+    
+    // Encriptar a senha antes de armazenar no banco de dados
+    const passwordHash = await bcrypt.hash(password, 10);
+    
+    // Inserir o usuário na tabela
+    await sql`INSERT INTO Users (name, email, password_hash) VALUES (${name}, ${email}, ${passwordHash});`;
+    revalidatePath('/api/user/get'); 
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
+ 
+  return NextResponse.json({ message: 'User created successfully' }, { status: 201 });
 }
