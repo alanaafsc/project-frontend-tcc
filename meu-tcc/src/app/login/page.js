395b9780178eba4d6b4ebac512c6a3aca82dd7eb
Alana Fernandes <48449273+alanaafsc@users.noreply.github.com>
2023-09-12 23:34:48 -0300

    att

--- meu-tcc/src/app/login/page.js
+++ meu-tcc/src/app/login/page.js
@@ -44,11 +44,7 @@ export default function Login() {
             const users = data.users.rows;
 
             const user = users.find(user => user.email === email);
-            console.log(users)
-            console.log(user)
-            console.log(email)
-            console.log(password)
-            console.log(user.password_hash)
+        
             if (user && bcrypt.compareSync(password, user.password_hash)) {
                 localStorage.setItem('userLoggedIn', 'true');
                 window.location.href = '/overview';
